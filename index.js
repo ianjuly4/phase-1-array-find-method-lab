705a7497a0b2b97043624a51a8a8d451ac342c22
Ian July <ianjuly4@gmail.com>
2023-09-23 09:51:51 -0600

    find lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+]
+function superbowlWin(wins) {
+  let result = null;
+  for (const record of wins) {
+    if (record.result === "W") {
+      return(record.year)
+    }
+  }
+}
